604adda087eceee1b48df606d547b841c5d7c160
vyskoutaris <vyskoutaris@hotmail.com>
2018-12-28 13:19:23 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,8 +1,8 @@
 
 var playlist = {
-artist1 = "song1",
-artist2 = "song2",
-artist3 = "song3"
+artist1: "song1",
+artist2: "song2",
+artist3: "song3"
 };
 
 playlist.artistName = [ "Phil Ochs", "Grimes"];
