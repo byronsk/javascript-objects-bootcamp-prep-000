eb66834feead5b1911ca9bdc724cb54c11004289
vyskoutaris <vyskoutaris@hotmail.com>
2018-12-28 13:20:48 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,8 +1,8 @@
 
 var playlist = {
-artist1: "song1",
-artist2: "song2",
-artist3: "song3"
+  artist1: "song1",
+  artist2: "song2",
+  artist3: "song3"
 };
 
 function updatePlaylist(thePlaylist, artistName, songTitle) {
@@ -10,3 +10,11 @@ function updatePlaylist(thePlaylist, artistName, songTitle) {
 }
 
 updatePlaylist(playlist, "artist4", "song4");
+
+function removeFromPlaylist(thePlaylist, artistName) {
+  delete thePlaylist[artistName];
+  return thePlaylist;
+}
+
+removeFromPlaylist(playlist, "artist2");
+
