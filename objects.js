8970118ccd3637635825eb4a820c8bec201fd7ca
vyskoutaris <vyskoutaris@hotmail.com>
2018-12-28 13:19:56 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,9 +5,6 @@ artist2: "song2",
 artist3: "song3"
 };
 
-playlist.artistName = [ "Phil Ochs", "Grimes"];
-playlist.songTitle = [ "My Bloody Valentine", "Slowdive"];
-
 function updatePlaylist(thePlaylist, artistName, songTitle) {
   return Object.assign({}, thePlaylist, { [artistName]: songTitle});
 }
